8330248288c89e60276fa5060da2d2f47c889d19
Jake Atwell <jake.atwell@gmail.com>
2019-03-13 12:02:50 -0400

    More explicit initialization option

--- symlink_script.sh
+++ symlink_script.sh
@@ -3,3 +3,13 @@ for i in `ls | grep -v symlink_script.sh`; do
     mv -i ~/.$i{,.bak}
     ln -rs $i ~/.$i
 done
+
+
+# still need to have backup for preexisting files
+
+
+# need to do bashrc
+#ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+#ln -s ~/.dotfiles/vimrc ~/.vimrc
+#ln -s ~/.dotfiles/vim ~/.vim
+#ln -s ~/.dotfiles/i3 ~/.i3
